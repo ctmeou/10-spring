9df5c772a284b010ee245e24aa77c21b2f325b40
ctmeou <ictmeousz@gmail.com>
2023-09-15 12:24:10 +0900

    himedia spring

---
+++ 02_spring-web-mvc/chap06-async/src/main/java/com/ohgiraffers/async/Chap06AsyncApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap06AsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap06AsyncApplication.class, args);
+    }
+
+}
--- 02_spring-web-mvc/chap07-thymeleaf/src/main/java/com/ohgiraffers/thymeleaf/LectureController.java
+++ 02_spring-web-mvc/chap07-thymeleaf/src/main/java/com/ohgiraffers/thymeleaf/LectureController.java
@@ -1,9 +1,16 @@
 package com.ohgiraffers.thymeleaf;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/lecture")
@@ -28,7 +35,42 @@ public class LectureController {
         mv.addObject("num", 1);
         mv.addObject("str", "복숭아");
 
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("홍길동", 20, '남', "서울시 종로구"));
+        memberList.add(new MemberDTO("유관순", 16, '여', "서울시 노원구"));
+        memberList.add(new MemberDTO("장보고", 30, '남', "서울시 성북구"));
+        memberList.add(new MemberDTO("신사임당", 40, '여', "서울시 서초구"));
+        mv.addObject("memberList", memberList);
+
         mv.setViewName("lecture/conditional");
+
+        return mv;
+
+    }
+
+    @GetMapping("/etc")
+    public ModelAndView etc(ModelAndView mv) {
+
+        SelectCriteria selectCriteria = new SelectCriteria(1, 10, 3);
+        mv.addObject(selectCriteria);
+
+        mv.addObject("member", new MemberDTO("황주희", 20, '여', "서울시 강북구"));
+
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("홍길동", 20, '남', "서울시 종로구"));
+        memberList.add(new MemberDTO("유관순", 16, '여', "서울시 노원구"));
+        memberList.add(new MemberDTO("장보고", 30, '남', "서울시 성북구"));
+        memberList.add(new MemberDTO("신사임당", 40, '여', "서울시 서초구"));
+        mv.addObject("memberList", memberList);
+
+        Map<String, MemberDTO> memberMap = new HashMap<>();
+        memberMap.put("m01", new MemberDTO("홍길동", 20, '남', "서울시 종로구"));
+        memberMap.put("m02", new MemberDTO("유관순", 16, '여', "서울시 노원구"));
+        memberMap.put("m03", new MemberDTO("장보고", 30, '남', "서울시 성북구"));
+        memberMap.put("m04", new MemberDTO("신사임당", 40, '여', "서울시 서초구"));
+        mv.addObject("memberMap", memberMap);
+
+        mv.setViewName("lecture/etc");
 
         return mv;
 
